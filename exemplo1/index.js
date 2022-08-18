dd4d8eb8b883ff561b8060b67ecfc3c0581b9beb
DR4CH3N <thehellc4t@gmail.com>
2022-08-18 08:34:53 -0300

    adicionados arquivos mais recentes

--- exemplo1/index.js
+++ exemplo1/index.js
@@ -1,35 +1,31 @@
-let dollar = 5.09;
-
-let dollar10 = 10 * dollar;
-
-console.log(`U$ 10 equivale a R$ ${dollar10}`);
-console.log('-------------------------------');
-
-let aluno = 'chaves';
-let nota1 = 7.58;
-let nota2 = 9.12;
+let dolar = 5.09;
+let dolar10 = 10 * dolar;
+console.log(`U$ 10 equivale a R$ ${dolar10}`);
+console.log('---------------------------------')
+let aluno = 'Chaves'
+let nota1 = 6.54;
+let nota2 = 5.31;
 let media = (nota1 + nota2) /2;
+// console.log(`O aluno ${aluno} tirou a nota: ${nota1} em Front-End, e no Back-End foi de: ${nota2} no final teve a media de ${media}`);
 
-console.log(nota1, nota2, media);
-console.log('---------------------');
-
+// Variável nao precisa ser declarada o valor imediatamente
 let situacao;
-if (media >=7) {
-    situacao = "aprovado";
-} else {
-    situacao = "reprovado";
+if(media>=7){
+    situacao = "Aprovado";
+}else{
+    situacao = "Reprovado";
 }
+// console.log(`O aluno ${aluno} alcançou a média de: ${media} está ${situacao}.`);
+console.log("O aluno "+aluno+" alcançou a média de: "+media+" e está "+situacao);
+console.log('----------------------------------------------------------------------');
 
-console.log(`o aluno ${aluno} alcancou media ${media} e esta ${situacao}.`)
 
-console.log('---------------------');
-
+// Criando um objeto.
 var dados = {
-    nome : 'astolfo',
-    idade : '24',
+    nome : "Gabriel",
+    idade : 22,
 
 }
 console.table(dados);
+// Acessando os dados do objeto.
 console.log(dados.nome);
-console.log(dados.idade);
-
---
+++ saves-aula/saves-cli.md
@@ -0,0 +1,13 @@
+<!-- Esse comando chama o arquivo através do terminal. -->
+# comandos cli
+- node (arquivo.extensão) Esse comando executa oque estamos chamando
+- type nul > (nome do arquivo)
+- mkdir Make a directory
+
+- CTRL+C Interrompe o processo de servidor
+
+
+<!-- Módulos são um conjunto de funções que acessamos dentro de um arquivo -->
+O arquivo .json é criado para que o JavaScript aceite modulos.
+
+O export default somente é chamado quando criamos precisamos chamar um único arquivo
