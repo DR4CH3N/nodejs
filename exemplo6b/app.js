5608b591e9afe87bdca349f9ae25cd65a471decc
DR4CH3N <thehellc4t@gmail.com>
2022-08-23 11:15:25 -0300

    configuração de server com nodejs

---
+++ exemplo6b/app.js
@@ -0,0 +1,26 @@
+import Express from "express";
+const app = Express();
+
+// configurando o motor de visualização com EJS
+app.set('view engine', 'ejs');
+
+
+// configurando rotas
+app.get('/', (req, res) => {
+    // res.send('urraah!')
+    let bandas = ['the weekend', 'freddie dredd', 'travis scott', 'asap rocky'];
+    res.render('index', {bandas});
+})
+
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+})
+app.get('/contato', (req, res) => {
+    res.render('contato');
+})
+
+
+// configurando o servidor
+app.listen(8080, () => {
+    console.log('server express rodando...');
+})
